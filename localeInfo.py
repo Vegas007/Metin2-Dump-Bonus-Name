a54019a38caaabfe6e87e49f76cb17e12ac33a82
Vegas007 <vegas_project1337@yahoo.com>
2019-04-06 00:36:42 +0300

    Update localeInfo.py

--- localeInfo.py
+++ localeInfo.py
@@ -4,14 +4,14 @@ def GetLetterCloseImageName():
 #2) Add after:
 import re
 import uiToolTip
-def FormatBonusNameString(affectType):
-	''' Return name of bonus (string) by a specific affect type.
+def FormatBonusNameString(bonusIndex):
+	''' Return name of bonus (string) by a specific bonus index.
 		param-ex: item.APPLY_RESIST_FIRE
 		return-ex: 'Fire Resistance'
 	'''
 	AFFECT_DICT = uiToolTip.ItemToolTip().AFFECT_DICT
-	if affectType in AFFECT_DICT:
-		return ' '.join(re.findall(pattern='[a-zA-Z]+', string=AFFECT_DICT[affectType](0), flags=0))
+	if bonusIndex in AFFECT_DICT:
+		return ' '.join(re.findall(pattern='[a-zA-Z]+', string=AFFECT_DICT[bonusIndex](0), flags=0))
 	return 'UNKNOWN_TYPE'
 	
 def FormatBonusNameDict():
@@ -20,6 +20,6 @@ def FormatBonusNameDict():
 		return-ex: {61: 'Strong against Sura', 100: 'Power of Fire', 108: 'Sword Defence Break'}
 	'''
 	result = {}
-	for affectType in uiToolTip.ItemToolTip().AFFECT_DICT.keys():
-		result.update({affectType: FormatBonusNameString(affectType)})
+	for bonusIndex in uiToolTip.ItemToolTip().AFFECT_DICT.keys():
+		result.update({bonusIndex: FormatBonusNameString(bonusIndex)})
 	return result
